9cc6a44909b3980fb784e6895f8d49a3c63b768c
YanPanMichael <yanpancsu@icloud.com>
2019-07-18 22:16:53 +0800

    add asserts

--- CNN.py
+++ CNN.py
@@ -22,6 +22,12 @@ y_train = np.array(raw_data_y)
 x = np.array([8.094, 3.366])
 
 def kNN_classify(k, X_train, y_train, x):
+  assert 1 <= k <= X_train.shape[0], "k must be vaild"
+  assert X_train.shape[0] == y_train.shape[0], ("the size"
+      + " of X_train must be equal to the size of y_train")
+  assert X_train.shape[1] == x.shape[0], ("the feature "
+      + "number of x must be equal to X_train")
+
   distances = [sqrt(np.sum((x_train - x) ** 2)) for x_train in X_train)]
   nestindexs = np.argsort(distances)
 
